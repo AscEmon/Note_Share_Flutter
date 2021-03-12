cd9520040e5df8821beb277400ee235d3f6de0f6
AscEmon <sayedemon53@gmail.com>
2021-03-12 21:31:18 +0600

    CachedNetwork add in ImageLoad and Uploading Page design modify

--- lib/Screens/UploadPage.dart
+++ lib/Screens/UploadPage.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'dart:async';
 import 'package:multi_image_picker/multi_image_picker.dart';
@@ -23,7 +24,7 @@ class _UploadPageState extends State<UploadPage> {
   String departMentName = "";
   String uploaderName;
   Asset asset;
-  bool isLoading=false;
+  bool isLoading = false;
 
   Future<void> pickImages() async {
     List<Asset> resultList = List<Asset>();
@@ -34,7 +35,7 @@ class _UploadPageState extends State<UploadPage> {
         enableCamera: true,
         selectedAssets: images,
         materialOptions: MaterialOptions(
-        actionBarTitle: "NoteShare",
+          actionBarTitle: "NoteShare",
         ),
       );
     } on Exception catch (e) {
@@ -60,9 +61,9 @@ class _UploadPageState extends State<UploadPage> {
 //Multiple image upload under uid
   Future handleUploadImage() async {
     setState(() {
-      isLoading=true;
+      isLoading = true;
     });
-    
+
     var firebaseUser = FirebaseAuth.instance.currentUser;
     var result = await FirebaseFirestore.instance
         .collection("Users")
@@ -90,8 +91,14 @@ class _UploadPageState extends State<UploadPage> {
         final UploadTask task =
             storageRef.child('$uploaderName/$i/').putFile(fileImageArray[i]);
         await task.then((picValue) async {
-           
-            
+          Fluttertoast.showToast(
+              msg: "UpLoading...",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 1,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0);
           // await picValue.ref.getDownloadURL().then((downloadUrl) {
           //   print("URL : " + downloadUrl);
           //      url=downloadUrl;
@@ -99,14 +106,13 @@ class _UploadPageState extends State<UploadPage> {
           // });
         });
       }
-       setState(() {
+      setState(() {
         print("calling");
-         images=null;
-         dropdownValue='Select Your Subject';
-         isLoading=false;
-      //fileImageArray.removeAt(i);
+        images = null;
+        dropdownValue = 'Select Your Subject';
+        isLoading = false;
+        //fileImageArray.removeAt(i);
       });
-     
     } catch (e) {
       print(e);
     }
@@ -125,105 +131,98 @@ class _UploadPageState extends State<UploadPage> {
                   fontWeight: FontWeight.bold)),
           centerTitle: true,
         ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+               GestureDetector(
+                 onTap: handleUploadImage,
+                  child: Container(
+                  height: 50,
+                  width: 180,
+                  child: Center(child: Text("Upload",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w600),)),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+              ),
+               ),
+              FloatingActionButton(
+                child: Icon(Icons.image),
+                onPressed: () {
+                  if (dropdownValue == "Select Your Subject") {
+                    Get.snackbar("Subject Select", "Please Select a  Subject",
+                        snackPosition: SnackPosition.BOTTOM,
+                        backgroundColor: Colors.red,
+                        colorText: Colors.white);
+                  } else {
+                    pickImages();
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
         body: Container(
             child: Column(
           children: [
             SizedBox(
               height: 8,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.red[300],
-                    borderRadius: BorderRadius.circular(10)),
-                height: 180,
-                width: Get.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      // crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        RaisedButton(
-                          onPressed: () {
-                            if (dropdownValue == "Select Your Subject") {
-                              Get.snackbar(
-                                  "Subject Select", "Please Select a  Subject",
-                                  snackPosition: SnackPosition.BOTTOM,
-                                  backgroundColor: Colors.red,
-                                  colorText: Colors.white);
-                            } else {
-                              pickImages();
-                            }
-                          },
-                          child: Text("Select"),
-                        ),
-                        SizedBox(
-                          width: 32,
-                        ),
-                        RaisedButton(
-                          onPressed: handleUploadImage,
-                          child: Text("Upload"),
-                        )
-                      ],
-                    ),
-                    DropdownButton<String>(
-                      value: dropdownValue,
-                      icon: Icon(Icons.arrow_drop_down),
-                      iconSize: 24,
-                      elevation: 16,
-                      style: TextStyle(color: Colors.deepPurple),
-                      underline: Container(
-                        height: 2,
-                        color: Colors.deepPurple,
-                      ),
-                      onChanged: (String newValue) {
-                        setState(() {
-                          dropdownValue = newValue;
-                          print(dropdownValue);
-                        });
-                      },
-                      items: <String>[
-                        'Select Your Subject',
-                        'Object Oriented Programming',
-                        'Mobile Application Development',
-                        'Data Structure',
-                        'Algorithm Analysis',
-                        'DataBase Maangement System',
-                        'Computer Network'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    )
-                  ],
-                ),
+            DropdownButton<String>(
+              isExpanded: true,
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(color: Colors.deepPurple,fontSize: 16),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurple,
               ),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                  print(dropdownValue);
+                });
+              },
+              items: <String>[
+                'Select Your Subject',
+                'Object Oriented Programming',
+                'Mobile Application Development',
+                'Data Structure',
+                'Algorithm Analysis',
+                'DataBase Maangement System',
+                'Computer Network'
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value,style: TextStyle(fontSize: 16),),
+                );
+              }).toList(),
             ),
             SizedBox(
               height: 16,
             ),
             Expanded(
               flex: 2,
-              child: isLoading==false? GridView.count(
-                crossAxisCount: 3,
-                children:images != null? List.generate(images.length, (index) {
-                  asset = images[index];
-                  return AssetThumb(
-                    asset: asset,
-                    width: 300,
-                    height: 300,
-                  );
-                }):[Text("")],
-              ):Center(child: CircularProgressIndicator(),)
-            ),
+              child: isLoading == false
+                  ? GridView.count(
+                      crossAxisCount: 3,
+                      children: images != null
+                          ? List.generate(images.length, (index) {
+                              asset = images[index];
+                              return AssetThumb(
+                                asset: asset,
+                                width: 300,
+                                height: 300,
+                              );
+                            })
+                          : [Text("")],
+                    )
+                  : Text(""),
+            )
           ],
         )),
       ),
